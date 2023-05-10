fa6cf4e6c623e0cf48952b73385229537085c8c3
Aniket <narsikaraniket@gmail.com>
2023-05-10 19:39:27 +0530

    all folders added

---
+++ src/main/java/controller/ReadServlet.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ReadServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Read User By ID</h2>");
+        out.println("<form action='/myapp/ReadServlet' method='POST'>");
+        out.println("User ID: <input type='text' name='userId'><br>");
+        out.println("<input type='submit' value='Submit'>");
+        out.println("</form>");
+        out.println("</body></html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String userId = request.getParameter("userId");
+        // code to retrieve user data from database using the user ID
+        // and display it in the response to the client
+        // ...
+    }
+}
